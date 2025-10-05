ace99aae591e0865ce7fb9d7c16ed5c8a5e83388
Asjadgit <naveedasjad840@gmail.com>
2025-10-05 21:36:22 +0500

    feat: todo app

--- Day2-TodoApp/src/App.jsx
+++ Day2-TodoApp/src/App.jsx
@@ -4,30 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])  // array to hold all tasks
 
+  function todolist(){
+    if(task.trim() === '') return task;
+    setTasks([...tasks,task]);
+    setTask(''); // clear the input
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Todo App</h1>
+        <input type='text' 
+              placeholder='Enter the task' 
+              value={task} 
+              onChange={(e) => setTask(e.target.value)}/>
+        <button onClick={todolist}>Submit</button>
+        <ul>
+            {tasks.map((t,index) => (
+              <li key={index}>{t}</li>
+            ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
