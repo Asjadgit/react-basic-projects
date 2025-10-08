784c54b1fde1403132441db76d04af40692d28b3
Asjadgit <naveedasjad840@gmail.com>
2025-10-08 22:22:15 +0500

    feat: using usestate the toggle swicth button to get rythm on react js

---
+++ Day3-SwitchButton/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [button,setButton] = useState(0);
+
+  function handleswitch(){
+    setButton(prev => (prev === 0 ? 1 : 0)); //get the previous value and chnage accordingly
+  }
+
+  return (
+    <>
+      <h1>Toggle Switch</h1>
+
+      <div
+        className={`toggle-container ${button === 1 ? "active" : ""}`}
+        onClick={handleswitch}
+      >
+        <div className="toggle-switch"></div>
+      </div>
+    </>
+  );
+}
+
+export default App
